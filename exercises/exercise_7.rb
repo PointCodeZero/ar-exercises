26c01375f908f268be4c4838885cdaa48e431d23
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-20 19:41:10 +0000

    add users input and force error to display all error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts 'What is the name of your store?'
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+
+new_store.errors.full_messages.each do |msg|
+  puts 'error: ', msg 
+end
