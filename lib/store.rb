56461a1d89fae22e8848a653d0c3de8336eca114
Lucas Cruz <lucas_ftc@hotmail.com>
2019-02-20 19:39:52 +0000

    add validation and bridge to employees

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validates :name, presence: true, length: { minimum: 3 } 
+  validates :annual_revenue, presence: true, length: {minimum: 0}, numericality: { only_integer: true }
 end
